c21a8a4564922e75b9770426a03eb34e4fdbe7bd
Ayman El-Geneidy <aelgeneidy01@yahoo.com>
2025-04-17 13:42:51 -0400

    Final version

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,8 +1,21 @@
-import { redirect } from 'react-router-dom';
+import { redirect, useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // Get the mode (either 'login' or 'signup')
+  const mode = searchParams.get('mode');
+
+  // On initial render, if no mode is present, set it to 'login'
+  useEffect(() => {
+    if (!mode) {
+      setSearchParams({ mode: 'login' });
+    }
+  }, [mode, setSearchParams]);
+
   return <AuthForm />;
 }
 
@@ -10,7 +23,7 @@ export default AuthenticationPage;
 
 export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
-  const mode = searchParams.get('mode') || 'login';
+  const mode = searchParams.get('mode') === 'login' ? 'login' : 'signup';
  
   if (mode !== 'login' && mode !== 'signup') {
     throw new Response(JSON.stringify({ message: 'Unsupported mode.' }, { status: 422 }));
@@ -21,8 +34,6 @@ export async function action({ request }) {
     email: data.get('email'),
     password: data.get('password')
   };
-  console.log(authData);
-  console.log(mode);
 
   const response = await fetch('http://localhost:5000/' + mode, {
     method: 'POST',
@@ -32,13 +43,21 @@ export async function action({ request }) {
     body: JSON.stringify(authData)
   });
 
-  if (response === 422 || response === 401) {
+  if (response.status === 422 || response.status === 401) {
     return response;
   }
 
   if (!response.ok) {
     throw new Response(JSON.stringify({ message: 'Could not authenticate user.' }, { status: 500 }));
   }
+
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem('token', token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem('expiration' + expiration.toISOString());
 
   return redirect('/');
 }
